c9c2fc33390c5f0957b2661f00f843b9ab2759ed
Sijan666 <666majharulislam@gmail.com>
2026-09-23 01:57:55 +0600

    feat: setup student routes

--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const { createStudent, getAllStudents, getStudentById, updateStudent, deleteStudent } = require('../controllers/studentController')
 const router = express.Router()
 
-router.post('/students')
-router.get('/students')
-router.get('/students/:id')
-router.patch('/students/:id')
-router.delete('/students/:id')
+router.post('/students',createStudent)
+router.get('/students',getAllStudents)
+router.get('/students/:id',getStudentById)
+router.patch('/students/:id',updateStudent)
+router.delete('/students/:id',deleteStudent)
 
 module.exports = router

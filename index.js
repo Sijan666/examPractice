30a56a56746a1aca6ae65c83aaa54d4d9e080ca2
Sijan666 <666majharulislam@gmail.com>
2026-09-22 09:49:19 +0600

    feat: add index configuration

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
-
+const studentRoute = require('./routes/studentRoute')
 
 app.use(express.json())
 
+app.use('/api/v1/students',studentRoute)
 
 
 let port = 5000

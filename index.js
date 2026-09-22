1cef650a7f2251e14982fc8e83d0c874c144bcfc
Sijan666 <666majharulislam@gmail.com>
2026-09-23 01:58:08 +0600

    refactor: update main entry point with routes and db

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+require("dotenv").config()
 const express = require('express')
 const app = express()
 const studentRoute = require('./routes/studentRoute')
+const mongoDb = require('./config/mongoDb')
 
 app.use(express.json())
 
+mongoDb()
+
 app.use('/api/v1/students',studentRoute)
 
-
-let port = 5000
+let port = process.env.port || 5000
 app.listen(port , ()=>{
     console.log(`Server is running on ${port}`);
 })

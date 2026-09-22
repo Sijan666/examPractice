1ab5857d161d0670bcc7da9126545c5c8fe490ed
Sijan666 <666majharulislam@gmail.com>
2026-09-22 09:49:45 +0600

    feat: add student model

--- models/studentSchema.js
+++ models/studentSchema.js
@@ -1,33 +1,32 @@
 const mongoose = require('mongoose')
-const {Schema} = mongoose
+const { Schema } = mongoose
 
 const studentSchema = new Schema({
-    name : {
-        type : String,
-        required : true
+    name: {
+        type: String,
+        required: true
     },
-    email : {
-        type : String,
-        required : true,
-        unique : true
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
-    phone : {
-        type : String,
-        required : true,
+    phone: {
+        type: String,
+        required: true,
     },
-    age : {
-        type : Number,
-        required : true,
+    age: {
+        type: Number,
+        required: true,
     },
-    isActive : {
-        type : Boolean,
-        default : true
+    isActive: {
+        type: Boolean,
+        default: true
     },
-    enrolledCourses : {
-        type : String,
-        enum : ['Full-Stack Development' , 'Front-End Development' , 'Backend Development']
-    }
+    enrolledCourses: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Course'
+    }]
 })
 
-
-module.exports = mongoose.model('Student' , studentSchema)
+module.exports = mongoose.model('Student', studentSchema)

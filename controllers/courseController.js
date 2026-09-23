b6f4b9dbb8fc586eafffb67420e728322fff162f
Sijan666 <666majharulislam@gmail.com>
2026-09-24 01:17:16 +0600

    feat: add course controller methods

---
+++ controllers/courseController.js
@@ -0,0 +1,90 @@
+const mongoose = require('mongoose')
+const Course = require('../models/courseSchema')
+
+// create course
+const createCourse = async (req, res) => {
+    try {
+        const { title, description, price, category, durationInMonths, isPublished } = req.body
+
+        if (!title || !price || !category || !durationInMonths) {
+            return res.status(400).json({
+                success: false,
+                message: "please fill all the required fields"
+            })
+        }
+
+        if (price <= 0) {
+            return res.status(400).json({
+                success: false,
+                message: 'price must be greater than 0'
+            })
+        }
+
+        if (durationInMonths < 1) {
+            return res.status(400).json({
+                success: false,
+                message: 'duration must be at least 1 month'
+            })
+        }
+
+        const existingCourse = await Course.findOne({ title, category })
+        if (existingCourse) {
+            return res.status(400).json({
+                success: false,
+                message: 'a course with this exact title and category already exists'
+            })
+        }
+
+        const newCourse = await new Course({
+            title,
+            description,
+            price,
+            category,
+            durationInMonths,
+            isPublished
+        }).save()
+
+        res.status(201).json({
+            success: true,
+            message: 'course successfully created',
+            data: newCourse
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'failed to create course',
+            error: error.message
+        })
+    }
+}
+
+const allcourse = async (req,res) => {
+    const allcourse = await Course.find({})
+    res.status(200).json({
+        success: true,
+        count: allcourse.length,
+        data: allcourse
+    })
+}
+
+const singlecourse = async (req,res) => {
+    const {id} = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({
+            success: false,
+            message: 'Invalid MongoDB ObjectId format'
+        })
+    }
+
+    const course = await Course.findById(id)
+    if (!course) {
+        return res.status(404).json({
+            success: false,
+            message: 'Course not found'
+        })
+    }
+}
+
+module.exports = { createCourse ,allcourse , singlecourse}

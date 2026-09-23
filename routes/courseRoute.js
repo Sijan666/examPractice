6db658a7aa04ddcfc9360a10278b715633090148
Sijan666 <666majharulislam@gmail.com>
2026-09-24 01:18:42 +0600

    feat: setup course routes

---
+++ routes/courseRoute.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const { createCourse, getCourseById, updateCourse, deleteCourse, allcourse, singlecourse } = require('../controllers/courseController')
+const router = express.Router()
+
+router.post('/createcourse',createCourse)
+router.get('/allcourse',allcourse)
+router.get('/singlecourse/:id',singlecourse)
+// router.patch('/updatecourse/:id',updateCourse)
+// router.delete('/deletecourse/:id',deleteCourse)
+
+module.exports = router

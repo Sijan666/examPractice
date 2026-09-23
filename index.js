0c3bed23fa05104e2dbf139c12499cc353ba907b
Sijan666 <666majharulislam@gmail.com>
2026-09-24 01:19:11 +0600

    feat: update student route logic

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 const express = require('express')
 const app = express()
 const studentRoute = require('./routes/studentRoute')
+const courseRoute = require('./routes/courseRoute')
 const mongoDb = require('./config/mongoDb')
 
 app.use(express.json())
@@ -9,6 +10,7 @@ app.use(express.json())
 mongoDb()
 
 app.use('/api/v1/students',studentRoute)
+app.use('/api/v1/courses',courseRoute)
 
 let port = process.env.port || 5000
 app.listen(port , ()=>{

f93ad28a609e86d21ef4ae8d210b219a4329513a
Sijan666 <666majharulislam@gmail.com>
2026-09-25 01:39:46 +0600

    feat: add dynamic route for student course enrollment

--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { createStudent, getAllStudents, getStudentById, updateStudent, deleteStudent } = require('../controllers/studentController')
+const { createStudent, getAllStudents, getStudentById, updateStudent, deleteStudent, enrollStudent } = require('../controllers/studentController')
 const router = express.Router()
 
 router.post('/createstudent',createStudent)
@@ -7,5 +7,7 @@ router.get('/allstudent',getAllStudents)
 router.get('/singlestudent/:id',getStudentById)
 router.patch('/updatestudent/:id',updateStudent)
 router.delete('/deletestudent/:id',deleteStudent)
+router.post('/:studentId/enroll/:courseId', enrollStudent)
+
 
 module.exports = router

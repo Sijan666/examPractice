20cf92159b3037cdec664e751b409978f378eec3
Majharul Islam <666majharulislam@gmail.com>
2026-09-24 15:14:54 +0600

    refactor : update router

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -5,7 +5,7 @@ const router = express.Router()
 router.post('/createcourse',createCourse)
 router.get('/allcourse',allcourse)
 router.get('/singlecourse/:id',singlecourse)
-// router.patch('/updatecourse/:id',updateCourse)
+router.patch('/updatecourse/:id',updateCourse)
 // router.delete('/deletecourse/:id',deleteCourse)
 
 module.exports = router
